5ac88235f97bf6c049ac7b52e4bbea549d850fa9
- <->
2023-01-12 19:30:43 +0300

    добавлен конструктолр карт

--- Homework13.Oop3/Data/Card.cs
+++ Homework13.Oop3/Data/Card.cs
@@ -4,4 +4,12 @@ public class Card
 {
     public string Name { get; set; }
     public decimal Money { get; set; }
+
+    public Card (string name, decimal money = 0)
+    {
+        Name = name;
+        Money = money;
+
+    }
+
 }
--- Homework13.Oop3/MainWindow.xaml.cs
+++ Homework13.Oop3/MainWindow.xaml.cs
@@ -14,8 +14,15 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        Storage.Clients.Add(new Client("Петя", new List<Card>() {new Card ()}));
-        Storage.Clients.Add(new Client("Вася", new List<Card>()));
+        Storage.Clients.Add(new Client("Петя", new List<Card>() 
+            {
+                new Card ("1236 4598 5896 4587", 1000), 
+                new Card ("1236 4785 5896 4758")
+            }));
+        Storage.Clients.Add(new Client("Вася", new List<Card>()
+            {
+                new Card ("1245 8965 1265 4589", 5_000_000)
+            }));
 
         Clients_Grid.ItemsSource = Storage.Clients;
     }
