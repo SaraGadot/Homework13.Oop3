2ec8c6d11520fb875d00dd95c99d3b069fef9a41
SaraGadot <eka0907@mail.ru>
2023-03-15 23:33:20 +0300

    Transfer<TMoney>

--- Homework13.Oop3/CardManager.cs
+++ Homework13.Oop3/CardManager.cs
@@ -3,10 +3,19 @@
 namespace Homework13.Oop3;
 internal class CardManager
 {
-    public static void Transfer(Card<decimal> fromCard, Card<decimal> toCard, decimal money)
+    public static void Transfer<TMoney>(Card<TMoney> fromCard, Card<TMoney> toCard, TMoney money) where TMoney:struct
     {
-        fromCard.Money = fromCard.Money - money;
-        toCard.Money = toCard.Money + money;
+        fromCard.Money = Minus(fromCard.Money, money);
+        toCard.Money = Plus(toCard.Money, money);
     }
+
+    private static TNumber Minus<TNumber>(TNumber a, TNumber b)
+    {
+        return (dynamic)a - b;
+    }
+    private static TNumber Plus<TNumber>(TNumber a, TNumber b)
+    {
+        return (dynamic)a + b;
+    } 
 }
 
