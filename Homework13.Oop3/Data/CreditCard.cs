37a3ae2aa8af1b14fdb15e2158072735c92d866d
- <->
2023-09-14 19:53:03 +0300

    CreditCard: Limit

--- Homework13.Oop3/Data/CreditCard.cs
+++ Homework13.Oop3/Data/CreditCard.cs
@@ -1,4 +1,5 @@
 ﻿using Homework13.Oop3.Interfaces;
+using System;
 
 namespace Homework13.Oop3.Data;
 
@@ -7,12 +8,14 @@ public class CreditCard<TMoney>: ICard<TMoney> where TMoney: struct
     public string Name { get; set; }
 
     public CardKind CardKind { get; set; } = CardKind.Credit;
+
+    public TMoney Limit { get; set; }
     public TMoney Money { get; private set; }
 
-    public CreditCard(string name, TMoney money)
+    public CreditCard(string name, TMoney limit)
     {
         Name = name;
-        Money = money;
+        Limit = limit;
     }
 
     public override string ToString()
@@ -22,6 +25,14 @@ public class CreditCard<TMoney>: ICard<TMoney> where TMoney: struct
 
     public void ChangeMoney(TMoney money)
     {
-        Money = (dynamic)Money + money;
+        var newMoney = (dynamic)Money + money;
+        if (newMoney < -(dynamic)Limit)
+        {
+            throw new Exception("Сумма кредитного счета не может быть меньше лимита");
+        }
+        else
+        {
+            Money = newMoney;
+        }
     }
 }
